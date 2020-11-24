c89c47313ab32c77c33db1a95f370c5a48cf2c97
Marcelo Ott <marcelobonowott@hotmail.com>
2020-11-24 11:52:13 -0300

    Atualização do codigo multi image button

--- Scripts/MultiImageButton.cs
+++ Scripts/MultiImageButton.cs
@@ -9,7 +9,7 @@ public class MultiImageButton : Button
     {
         get
         {
-            if(m_graphics == null)
+            if (m_graphics == null)
             {
                 m_graphics = targetGraphic.transform.parent.GetComponentsInChildren<Graphic>();
             }
@@ -20,45 +20,45 @@ public class MultiImageButton : Button
     protected override void DoStateTransition(SelectionState state, bool instant)
     {
         Color color;
-        switch(state)
+        switch (state)
         {
-            case Selectable.SelectionState.Normal:
-            color = this.colors.normalColor;
-            break;
-            case Selectable.SelectionState.Highlighted:
-            color = this.colors.highlightedColor;
-            break;
-            case Selectable.SelectionState.Pressed:
-            color = this.colors.pressedColor;
-            break;
-            case Selectable.SelectionState.Disabled:
-            color = this.colors.disabledColor;
-            break;
-            default:
-            color = Color.black;
-            break;
-        }
-        if(base.gameObject.activeInHierarchy)
-        {
-            switch(this.transition)
-            {
-                case Selectable.Transition.ColorTint:
-                ColorTween(color * this.colors.colorMultiplier, instant);
+            case SelectionState.Normal:
+                color = this.colors.normalColor;
                 break;
+            case SelectionState.Highlighted:
+                color = this.colors.highlightedColor;
+                break;
+            case SelectionState.Pressed:
+                color = this.colors.pressedColor;
+                break;
+            case SelectionState.Disabled:
+                color = this.colors.disabledColor;
+                break;
+            default:
+                color = Color.black;
+                break;
+        }
+        if (base.gameObject.activeInHierarchy)
+        {
+            switch (this.transition)
+            {
+                case Transition.ColorTint:
+                    ColorTween(color * this.colors.colorMultiplier, instant);
+                    break;
                 default:
-                throw new NotSupportedException();
+                    break;
             }
         }
     }
 
     private void ColorTween(Color targetColor, bool instant)
     {
-        if(this.targetGraphic == null)
+        if (this.targetGraphic == null)
         {
             return;
         }
 
-        foreach(Graphic g in this.Graphics)
+        foreach (Graphic g in this.Graphics)
         {
             g.CrossFadeColor(targetColor, (!instant) ? this.colors.fadeDuration : 0f, true, true);
         }
