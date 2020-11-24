db42dbe2c4302efd1014d0f43ec3c4be866cabea
Marcelo Ott <marcelobonowott@hotmail.com>
2020-11-24 19:01:12 -0300

    Adicionado on pointer click

--- Scripts/PointerHandler.cs
+++ Scripts/PointerHandler.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
-public class PointerHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class PointerHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler
 {
     public Events<PointerEventData> onPointerDown = new Events<PointerEventData>();
     public Events<PointerEventData> onPointerUp = new Events<PointerEventData>();
+    public Events<PointerEventData> onPointerClick = new Events<PointerEventData>();
 
     public void OnPointerDown(PointerEventData pointerEventData) => onPointerDown.Trigger(pointerEventData);
     public void OnPointerUp(PointerEventData pointerEventData) => onPointerUp.Trigger(pointerEventData);
+    public void OnPointerClick(PointerEventData eventData) => onPointerClick.Trigger(eventData);
 }
