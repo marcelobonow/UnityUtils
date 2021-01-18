fcd34f08a52a5ba6b9ddc3f0716f87c67ffdd7d6
Marcelo Ott <marcelobonowott@hotmail.com>
2021-01-18 16:10:00 -0300

    Adicionado scene utils

---
+++ Scripts/SceneUtils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine.SceneManagement;
+
+public class SceneUtils
+{
+    public static string GetSceneName(int sceneBuildIndex)
+    {
+        var scenePath = SceneUtility.GetScenePathByBuildIndex(sceneBuildIndex);
+        var sceneNameStart = scenePath.LastIndexOf("/", StringComparison.Ordinal) + 1;
+        var sceneNameEnd = scenePath.LastIndexOf(".", StringComparison.Ordinal);
+        var sceneNameLength = sceneNameEnd - sceneNameStart;
+        return scenePath.Substring(sceneNameStart, sceneNameLength);
+    }
+}
