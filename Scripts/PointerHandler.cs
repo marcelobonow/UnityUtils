e1d5edd448f2f7d76b095f502f4baa5705370f4c
Marcelo Ott <marcelobonowott@hotmail.com>
2021-02-08 17:02:05 -0300

    Adicionado eventos para pointer enter e pointer exit

--- Scripts/PointerHandler.cs
+++ Scripts/PointerHandler.cs
@@ -3,13 +3,18 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
-public class PointerHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler
+public class PointerHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public Events<PointerEventData> onPointerDown = new Events<PointerEventData>();
     public Events<PointerEventData> onPointerUp = new Events<PointerEventData>();
     public Events<PointerEventData> onPointerClick = new Events<PointerEventData>();
+    public Events<PointerEventData> onPointerEnter = new Events<PointerEventData>();
+    public Events<PointerEventData> onPointerExit = new Events<PointerEventData>();
 
     public void OnPointerDown(PointerEventData pointerEventData) => onPointerDown.Trigger(pointerEventData);
     public void OnPointerUp(PointerEventData pointerEventData) => onPointerUp.Trigger(pointerEventData);
     public void OnPointerClick(PointerEventData eventData) => onPointerClick.Trigger(eventData);
+
+    public void OnPointerEnter(PointerEventData eventData) => onPointerEnter.Trigger(eventData);
+    public void OnPointerExit(PointerEventData eventData) => onPointerExit.Trigger(eventData);
 }
