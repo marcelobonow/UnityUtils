0ec6f1d8f29c785017f6de96edbbe7f9345da83f
Marcelo Ott <marcelobonowott@hotmail.com>
2021-02-22 16:58:55 -0300

    Adicionado ordenação de pontos de rect

---
+++ Scripts/ImageUtils.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ImageUtils
+{
+    ///Returns in order bottom left, bottom right, top right and top left
+    public static List<Vector2> OrderPoints(List<Vector2> points)
+    {
+        var squareCenter = new Vector2(points.Sum(point => point.x), points.Sum(point => point.y)) / points.Count;
+        return points.OrderBy(point => Mathf.Atan2(point.y - squareCenter.y, point.x - squareCenter.x)).ToList();
+    }
+}
