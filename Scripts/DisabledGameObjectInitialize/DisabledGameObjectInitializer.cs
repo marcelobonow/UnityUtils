e29efba380c2ed285c9f51adaf818828cd94fb23
Marcelo Ott <marcelobonowott@hotmail.com>
2021-03-24 18:43:11 -0300

    Script para inicializar objetos desligados

---
+++ Scripts/DisabledGameObjectInitialize/DisabledGameObjectInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DisabledGameObjectInitializer : MonoBehaviour
+{
+    private void Awake()
+    {
+        var scripts = new List<IDisabledGameObjectInitializable>();
+        var scene = SceneManager.GetActiveScene();
+
+        var rootObjects = scene.GetRootGameObjects();
+
+        foreach (var go in rootObjects)
+            scripts.AddRange(go.GetComponentsInChildren<IDisabledGameObjectInitializable>(true));
+        foreach (var script in scripts)
+            script.Initialize();
+    }
+}
