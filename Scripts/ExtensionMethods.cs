883eee3133477ddc5b1a9df6c3c3bc22c9913b6f
Diovan Marques <diovanmarques@gmail.com>
2021-05-14 21:30:54 -0300

    Método de extensão para retornar a string entre duas partes

--- Scripts/ExtensionMethods.cs
+++ Scripts/ExtensionMethods.cs
@@ -72,6 +72,20 @@ public static class ExtensionMethods
             return false;
         }
     }
+
+    public static string GetBetween(this string strSource, string strStart, string strEnd)
+    {
+        if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+        {
+            int Start, End;
+            Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+            End = strSource.IndexOf(strEnd, Start);
+            return strSource.Substring(Start, End - Start);
+        }
+
+        return "";
+    }
+
     public static Vector3 Clamp(this Vector3 vector3, Vector3 min, Vector3 max)
     {
         Vector3 result;
