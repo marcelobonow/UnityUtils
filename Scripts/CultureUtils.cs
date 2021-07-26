0bb608140696947fcb71ad2f071161e2b5c312d3
Marcelo Ott <marcelobonowott@hotmail.com>
2021-07-26 17:35:21 -0300

    Metodo para comparar strings ignorando case e acentos, e metodo para converter texto hex para cor

--- Scripts/CultureUtils.cs
+++ Scripts/CultureUtils.cs
@@ -4,7 +4,8 @@ using System.Globalization;
 using System.Linq;
 using UnityEngine;
 
-public class CultureUtils : MonoBehaviour {
+public class CultureUtils : MonoBehaviour
+{
 
     public static CultureInfo GetCurrentCultureInfo()
     {
@@ -12,4 +13,7 @@ public class CultureUtils : MonoBehaviour {
         CultureInfo correspondingCultureInfo = CultureInfo.GetCultures(CultureTypes.AllCultures).FirstOrDefault(x => x.EnglishName.Equals(currentLanguage.ToString()));
         return CultureInfo.CreateSpecificCulture(correspondingCultureInfo.TwoLetterISOLanguageName);
     }
+
+    public static bool CompareStringInvariantCulture(string string1, string string2) =>
+        string.Compare(string1, string2, CultureInfo.InvariantCulture, CompareOptions.IgnoreNonSpace | CompareOptions.IgnoreSymbols | CompareOptions.IgnoreCase) == 0;
 }
--- Scripts/ImageUtils.cs
+++ Scripts/ImageUtils.cs
@@ -11,4 +11,17 @@ public class ImageUtils
         var squareCenter = new Vector2(points.Sum(point => point.x), points.Sum(point => point.y)) / points.Count;
         return points.OrderBy(point => Mathf.Atan2(point.y - squareCenter.y, point.x - squareCenter.x)).ToList();
     }
+
+    public static Color HexToColor(string hex)
+    {
+        hex = hex.Replace("0x", ""); ///Remove 0x no caso de estar formatado 0xFFFFFF
+        hex = hex.Replace("#", ""); ///Remove # no caso de estar formatado #FFFFFF
+        var r = byte.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+        var g = byte.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
+        var b = byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
+
+        ///Só checa alpha se tiver caracteres, se não assume completamente opaco
+        var a = hex.Length == 8 ? byte.Parse(hex.Substring(6, 2), System.Globalization.NumberStyles.HexNumber) : (byte)255;
+        return new Color32(r, g, b, a);
+    }
 }
