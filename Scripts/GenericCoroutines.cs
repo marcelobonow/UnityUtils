048cbc360e750cf7b5138dad96902f22186a8278
Marcelo Ott <marcelobonowott@hotmail.com>
2021-10-25 19:34:10 -0300

    Metodo para esperar por frames

--- Scripts/GenericCoroutines.cs
+++ Scripts/GenericCoroutines.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 
 public class GenericCoroutines
 {
+    public static Coroutine DoAfterTime(float time, Action effect, MonoBehaviour mono) => mono.StartCoroutine(DoAfterTime(time, effect));
     private static IEnumerator DoAfterTime(float time, Action effect)
     {
         yield return WaitForSeconds(time);
         effect?.Invoke();
     }
-    public static Coroutine DoAfterTime(float time, Action effect, MonoBehaviour mono) => mono.StartCoroutine(DoAfterTime(time, effect));
+
+    public static Coroutine DoAfterFrames(int frames, Action effect, MonoBehaviour mono) => mono.StartCoroutine(DoAfterFrames(frames, effect));
+    private static IEnumerator DoAfterFrames(int frames, Action effect)
+    {
+        for (int i = 0; i < frames; i++)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        effect?.Invoke();
+    }
 
     private static IEnumerator DoAfterRealTime(float time, Action effect)
     {
