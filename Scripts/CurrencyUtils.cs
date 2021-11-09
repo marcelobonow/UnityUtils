b261881d2ccfba02cec01aaf5cb6037f7c49f375
Marcelo Ott <marcelobonowott@hotmail.com>
2021-11-09 17:53:42 -0300

    Adicionado opção em como retornar string quando não tem valor

--- Scripts/CurrencyUtils.cs
+++ Scripts/CurrencyUtils.cs
@@ -6,5 +6,6 @@ using UnityEngine;
 public class CurrencyUtils
 {
     public static string GetPriceString(long cents) => string.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:C}", cents / 100m);
-    public static string GetHumanPrice(long cents) => cents == 0 ? "Sem Custo" : GetPriceString(cents);
+    public static string GetHumanPrice(long cents, string noValueString = "Sem Custo") =>
+        cents == 0 && noValueString != null ? noValueString : GetPriceString(cents);
 }
