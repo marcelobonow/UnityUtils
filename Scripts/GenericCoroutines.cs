bc5f17ccc9fdbb95e6a14cd8cc7e9b9e608ea2ce
Marcelo Bonow Ott <marcelobonowott@hotmail.com>
2024-10-08 18:34:51 -0300

    Adicioando metodo para fazer animação

--- Scripts/GenericCoroutines.cs
+++ Scripts/GenericCoroutines.cs
@@ -5,48 +5,53 @@ using UnityEngine;
 
 public class GenericCoroutines
 {
-    public static Coroutine DoAfterTime(float time, Action effect, MonoBehaviour mono) => mono.StartCoroutine(DoAfterTime(time, effect));
-    private static IEnumerator DoAfterTime(float time, Action effect)
-    {
-        yield return WaitForSeconds(time);
-        effect?.Invoke();
-    }
+  public static Coroutine DoAfterTime(float time, Action effect, MonoBehaviour mono) => mono.StartCoroutine(DoAfterTime(time, effect));
+  private static IEnumerator DoAfterTime(float time, Action effect)
+  {
+    yield return WaitForSeconds(time);
+    effect?.Invoke();
+  }
 
-    public static Coroutine DoAfterFrames(int frames, Action effect, MonoBehaviour mono) => mono.StartCoroutine(DoAfterFrames(frames, effect));
-    private static IEnumerator DoAfterFrames(int frames, Action effect)
+  public static Coroutine DoAfterFrames(int frames, Action effect, MonoBehaviour mono) => mono.StartCoroutine(DoAfterFrames(frames, effect));
+  private static IEnumerator DoAfterFrames(int frames, Action effect)
+  {
+    for (int i = 0; i < frames; i++)
     {
-        for (int i = 0; i < frames; i++)
-        {
-            yield return new WaitForEndOfFrame();
-        }
-        effect?.Invoke();
+      yield return new WaitForEndOfFrame();
     }
+    effect?.Invoke();
+  }
 
-    public static Coroutine DoOnEndOfFrame(Action effect, MonoBehaviour mono) => mono.StartCoroutine(DoOnEndOfFrame(effect));
+  private static IEnumerator DoAfterRealTime(float time, Action effect)
+  {
+    yield return WaitForSecondsRealtime(time);
+    effect?.Invoke();
+  }
 
-    private static IEnumerator DoOnEndOfFrame(Action effect)
+  public static IEnumerator WaitForSeconds(float seconds)
+  {
+    var startTime = Time.time;
+    while (Time.time < startTime + seconds)
+      yield return null;
+  }
+
+  public static IEnumerator WaitForSecondsRealtime(float seconds)
+  {
+    var startTime = Time.realtimeSinceStartup;
+    while (Time.realtimeSinceStartup < startTime + seconds)
+      yield return null;
+  }
+
+  ///Can call with 1 value two times
+  public static IEnumerator Animate(float duration, Action<float> effect, Action onFinish)
+  {
+    var startTime = Time.time;
+    while (Time.time < startTime + duration)
     {
-        yield return new WaitForEndOfFrame();
-        effect?.Invoke();
+      var step = (Time.time - startTime) / duration;
+      effect?.Invoke(step);
+      yield return null;
     }
-
-    private static IEnumerator DoAfterRealTime(float time, Action effect)
-    {
-        yield return WaitForSecondsRealtime(time);
-        effect?.Invoke();
-    }
-
-    public static IEnumerator WaitForSeconds(float seconds)
-    {
-        var startTime = Time.time;
-        while (Time.time < startTime + seconds)
-            yield return null;
-    }
-
-    public static IEnumerator WaitForSecondsRealtime(float seconds)
-    {
-        var startTime = Time.realtimeSinceStartup;
-        while (Time.realtimeSinceStartup < startTime + seconds)
-            yield return null;
-    }
+    onFinish?.Invoke();
+  }
 }
