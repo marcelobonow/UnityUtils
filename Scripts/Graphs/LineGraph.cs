c4b22e3c3aa30290edaa1ace62ec6bf7da8d4205
Marcelo Bonow Ott <marcelobonowott@hotmail.com>
2025-01-29 18:03:26 -0300

    Removido usings desnecessários

--- Scripts/Graphs/LineGraph.cs
+++ Scripts/Graphs/LineGraph.cs
@@ -1,9 +1,4 @@
-using System;
-using System.CodeDom;
-using System.Collections.Generic;
-using System.Drawing.Text;
-using System.Linq;
-using System.Net.NetworkInformation;
+﻿using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
